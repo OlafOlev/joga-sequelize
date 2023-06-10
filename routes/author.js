8acf11b07b0c9c21b4fe214049c3e01fc9688151
OlafOlev <oalfolev@gmail.com>
2023-06-10 11:37:32 +0300

    Joga sequalize

---
+++ routes/author.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const AuthorController = require('../controllers/admin/author')
+
+router.get('/', AuthorController.getAllAuthors);
+router.get('/:id', AuthorController.getAuthorById)
+
+module.exports = router;
